b51572a8c4b2998a05fd31cd127c016e0ffbb75a
DakotagM <teamtennis98@gmail.com>
2021-09-13 16:41:45 -0500

    Player Object Created
    Setup Player Object
    Started Player Controller

---
+++ ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+/****
+ * Created by: Dakota Mitchell
+ * Date: Sept 13,2021
+ * 
+ * Last Edited by:
+ * Last updated: Sept 13,2021
+ * 
+ * Controls Player Movement
+ */
+
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
